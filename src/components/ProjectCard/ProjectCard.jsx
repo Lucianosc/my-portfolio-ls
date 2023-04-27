a3ba9220f248aa271ff41282897023b5ce4aa6da
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-27 20:49:07 -0300

    Update on ProjectCard component

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import styles from './ProyectCard.module.scss'
+import React from "react";
+import styles from "./ProjectCard.module.scss";
 
-export default function ProyectCard({
+export default function ProjectCard({
   project: { id, image, description, title },
 }) {
   return (
-    <div key={id} className={`${styles['project-card']}`}>
+    <div key={id} className={`${styles["project-card"]}`}>
+      <button>view project</button>
       <img src={image} alt={title} />
       {/* <div className={`${styles['project-info']}`}>
         <h2>{title}</h2>
         <p>{description}</p>
       </div> */}
     </div>
-  )
+  );
 }
