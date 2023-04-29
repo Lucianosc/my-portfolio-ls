ddcc033027ff445b7b50f3befebc35935776f0e3
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-28 21:22:36 -0300

    Changes in SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,35 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { logo } from '../../assets'
-import { LinkedInLogo, GitHubLogo, HamburgerMenu } from '../../assets'
-import { useNavigate, useLocation } from 'react-router-dom'
-import { navlinks } from '../../constants/navlinks'
-import styles from './SideBar.module.scss'
+import React, { useState, useEffect, useRef } from "react";
+import { LinkedInLogo, GitHubLogo, HamburgerMenu, logo } from "../../assets";
+import { useNavigate, useLocation } from "react-router-dom";
+import { navlinks } from "../../constants/navlinks";
+import styles from "./SideBar.module.scss";
+import Overlay from "../Overlay/Overlay";
 
 export default function SideBar() {
-  const navigate = useNavigate()
-  const location = useLocation()
-  const drawerRef = useRef()
-  const [toggleDrawer, setToggleDrawer] = useState(false)
+  const navigate = useNavigate();
+  const location = useLocation();
+  const drawerRef = useRef();
+  const [toggleDrawer, setToggleDrawer] = useState(false);
 
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutsideDrawer, true)
+  // useEffect(() => {
+  //   document.addEventListener('click', handleClickOutsideDrawer, true)
 
-    return () => {
-      document.removeEventListener('click', handleClickOutsideDrawer, true)
-    }
-  }, [toggleDrawer])
+  //   return () => {
+  //     document.removeEventListener('click', handleClickOutsideDrawer, true)
+  //   }
+  // }, [toggleDrawer])
 
-  const handleClickOutsideDrawer = (e) => {
-    if (toggleDrawer && !drawerRef.current.contains(e.target)) {
-      setToggleDrawer(false)
-    }
-  }
+  // const handleClickOutsideDrawer = (e) => {
+  //   if (toggleDrawer && !drawerRef.current.contains(e.target)) {
+  //     setToggleDrawer(false)
+  //   }
+  // }
 
   return (
     <menu>
       {/* mobile nav */}
-      <div className={`${styles['mobile-nav']}`}>
-        <div className={`${styles['hamburger-container']}`}>
+      <div className={`${styles["mobile-nav"]}`}>
+        <div className={`${styles["hamburger-container"]}`}>
           <a onClick={() => setToggleDrawer((prev) => !prev)}>
             <HamburgerMenu width="32px" height="32px" />
           </a>
@@ -38,32 +38,32 @@ export default function SideBar() {
 
       {/* desktop nav */}
       <div
-        className={`${styles['side-nav']} ${toggleDrawer && styles['toggled']}`}
+        className={`${styles["side-nav"]} ${toggleDrawer && styles["toggled"]}`}
         ref={drawerRef}
       >
-        <div className={`${styles['logo']}`}>
+        <div className={`${styles["logo"]}`}>
           <div>
-            <a onClick={() => navigate('/')}>
+            <a onClick={() => navigate("/")}>
               <img src={logo} alt="logo" />
             </a>
           </div>
           <span>Web Developer</span>
         </div>
-        <nav className={`${styles['nav-links']}`}>
+        <nav className={`${styles["nav-links"]}`}>
           {navlinks.map(({ name, link }) => (
             <a
               key={name}
               onClick={() => {
-                navigate(link)
-                setToggleDrawer(false)
+                navigate(link);
+                setToggleDrawer(false);
               }}
-              className={`${link === location.pathname && styles['selected']}`}
+              className={`${link === location.pathname && styles["selected"]}`}
             >
               {name}
             </a>
           ))}
         </nav>
-        <div className={`${styles['social-media']}`}>
+        <div className={`${styles["social-media"]}`}>
           <a
             href="https://linkedin.com/in/luciano-scaminaci-605132220/"
             target="_blank"
@@ -75,8 +75,8 @@ export default function SideBar() {
           </a>
         </div>
       </div>
-      {/* blur screen */}
-      <div className={`${toggleDrawer && styles['blur-screen']}`}></div>
+      {/* Overlay*/}
+      {toggleDrawer && <Overlay handleClick={() => setToggleDrawer(false)} />}
     </menu>
-  )
+  );
 }
