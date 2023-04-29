600c6d3c3381af24cc4823caa07d6ddf2d17509a
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-28 21:22:27 -0300

    Changes in Projects & ProjectCard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import styles from "./ProjectCard.module.scss";
 
 export default function ProjectCard({
+  handleClick,
   project: { id, image, description, title },
 }) {
   return (
-    <div key={id} className={`${styles["project-card"]}`}>
+    <div
+      key={id}
+      className={`${styles["project-card"]}`}
+      onClick={() => handleClick()}
+    >
       <button>view project</button>
       <img src={image} alt={title} />
       {/* <div className={`${styles['project-info']}`}>
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./Projects.module.scss";
-import { ProjectCard } from "../../components";
+import { ProjectCard, Modal, Overlay } from "../../components";
 import { projects, uniProjects } from "../../constants/projects";
 
 export default function Projects() {
+  const [showModal, setShowModal] = useState(false);
+  const [projectData, setProjectData] = useState();
+  const modalRef = useRef();
+
+  const handleProyectClick = (project) => {
+    setProjectData(project);
+    setShowModal(true);
+  };
+
   return (
     <div className={`${styles["main-container"]}`}>
       <div className={`${styles["projects"]}`}>
         <h1>Projects</h1>
         <div className={`${styles["projects-list"]}`}>
           {projects.map((project) => (
-            <ProjectCard project={project} key={project.id} />
+            <ProjectCard
+              project={project}
+              key={project.id}
+              handleClick={() => handleProyectClick(project)}
+            />
           ))}
         </div>
       </div>
@@ -18,10 +31,22 @@ export default function Projects() {
         <h1>University projects</h1>
         <div className={`${styles["projects-list"]}`}>
           {uniProjects.map((project) => (
-            <ProjectCard project={project} key={project.id} />
+            <ProjectCard
+              project={project}
+              key={project.id}
+              handleClick={() => handleProyectClick(project)}
+            />
           ))}
         </div>
       </div>
+      {showModal && (
+        <>
+          <div ref={modalRef}>
+            <Modal projectData={projectData} />
+          </div>
+          <Overlay handleClick={() => setShowModal(false)} />
+        </>
+      )}
     </div>
   );
 }
