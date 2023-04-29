67d45b921ca733f5c7f5eb28d3247c8863db18ed
Lucianosc <scaminaci.luciano@gmail.com>
2023-04-28 21:21:05 -0300

    Added Modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,6 @@
+import React, { useRef } from "react";
+import styles from "./Modal.module.scss";
+
+export default function Modal() {
+  return <div className={`${styles["container"]}`}>Modal</div>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as SideBar } from "./SideBar/SideBar";
 export { default as ProjectCard } from "./ProjectCard/ProjectCard";
+export { default as Modal } from "./Modal/Modal";
 export { default as Overlay } from "./Overlay/Overlay";
+
 
