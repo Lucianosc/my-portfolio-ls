7d60d7449920c376e5d0361a4dbb5187d28974e8
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-02 20:34:05 -0300

    changes in projects content

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -3,24 +3,27 @@ import { crowdfundingDesktop, crowdfundingMobile } from "../assets";
 export const projects = [
   {
     id: 1,
-    title: "Crowdfunding Web3 App",
+    title: "",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor dui in faucibus consectetur.",
+      "<p>This is a crowdfunding web 3 application that allows users to create and fund campaigns through interaction with a smart contract on Ethereum Goerli testnet. This web application is designed to provide a seamless experience for users looking to raise funds for their projects using blockchain technology.</p><p>Users can easily create campaigns for funding, set funding goals and deadlines, and receive donations from contributors. Users can also view all the campaigns that are currently in the smart contract, providing them with visibility into the latest projects seeking funding.</p><p>In addition, the application allows users to view all the campaigns that belong to the current connected wallet, providing them with a personalized view of their own campaigns and enabling them to manage their campaigns efficiently.</p><p>This project was developed with a focus on usability, ensuring that users can easily interact with the smart contract and make secure transactions. The crowdfunding web 3 application is an exciting new development in the world of decentralized finance and is an excellent example of the potential of blockchain technology to revolutionize traditional fundraising methods.</p>",
     image: crowdfundingDesktop,
+    link: "https://google.com",
   },
   {
     id: 2,
     title: "Project 2",
     description:
-      "Pellentesque quis justo vel dolor dictum malesuada. Aliquam id faucibus lectus.",
+      "<p>Pellentesque quis justo vel dolor dictum malesuada. Aliquam id faucibus lectus.</p>",
     image: "https://via.placeholder.com/150x150",
+    link: "https://google.com",
   },
   {
     id: 3,
     title: "Project 3",
     description:
-      "Nam aliquet nunc eget orci dignissim, vel venenatis justo imperdiet.",
+      "<p>Nam aliquet nunc eget orci dignissim, vel venenatis justo imperdiet.</p>",
     image: "https://via.placeholder.com/150x150",
+    link: "https://google.com",
   },
 ];
 
@@ -31,6 +34,7 @@ export const uniProjects = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor dui in faucibus consectetur.",
     image: crowdfundingDesktop,
+    link: "https://google.com",
   },
   {
     id: 2,
@@ -38,6 +42,7 @@ export const uniProjects = [
     description:
       "Pellentesque quis justo vel dolor dictum malesuada. Aliquam id faucibus lectus.",
     image: "https://via.placeholder.com/150x150",
+    link: "https://google.com",
   },
   {
     id: 3,
@@ -45,5 +50,6 @@ export const uniProjects = [
     description:
       "Nam aliquet nunc eget orci dignissim, vel venenatis justo imperdiet.",
     image: "https://via.placeholder.com/150x150",
+    link: "https://google.com",
   },
 ];
