352f83e90daa9ed83926a98d725a4edbac817cda
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-02 20:37:05 -0300

    Changes in Modal & Overlay

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,30 @@
 import React, { useRef } from "react";
 import styles from "./Modal.module.scss";
+import { CloseIcon } from "../../assets";
 
-export default function Modal() {
-  return <div className={`${styles["container"]}`}>Modal</div>;
+export default function Modal({
+  handleClick,
+  projectData: { id, title, description, image, link },
+}) {
+  // console.log(projectData)
+  return (
+    <div className={`${styles["container"]}`}>
+      <button onClick={() => handleClick()}>
+        <CloseIcon />
+      </button>
+      <div className={`${styles["img-container"]}`}>
+        <img src={image} />
+        {link && (
+          <a target="_blank" href={link}>
+            Visit Website
+          </a>
+        )}
+      </div>
+
+      <div className={`${styles["text-container"]}`}>
+        {title && <h2>{title}</h2>}
+        <div dangerouslySetInnerHTML={{ __html: description }} />
+      </div>
+    </div>
+  );
 }
