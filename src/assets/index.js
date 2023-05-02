67cb16f8a5b8e2c778c496b7384f51fda1eb772d
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-02 20:34:41 -0300

    Added CloseIcon

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,3 +7,4 @@ export { logo, crowdfundingDesktop, crowdfundingMobile };
 export { default as GitHubLogo } from "./GitHubLogo.jsx";
 export { default as LinkedInLogo } from "./LinkedInLogo.jsx";
 export { default as HamburgerMenu } from "./HamburgerMenu.jsx";
+export { default as CloseIcon } from "./CloseIcon.jsx";
