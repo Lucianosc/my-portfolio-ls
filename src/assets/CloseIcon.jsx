7228d059615c2c4bc10113589d6f392d0fcea2c6
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-19 15:46:43 -0300

    Changes in CloseIcon

--- src/assets/CloseIcon.jsx
+++ src/assets/CloseIcon.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
-export default function CloseIcon() {
+export default function CloseIcon({ fill, width, height }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width}
+      height={height}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="square"
-      strokeLinejoin="square"
     >
-      <circle cx="12" cy="12" r="10" />
-      <line x1="15" y1="9" x2="9" y2="15" />
-      <line x1="9" y1="9" x2="15" y2="15" />
+      <g id="close">
+        <path
+          id="Vector"
+          d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
     </svg>
   );
 }
