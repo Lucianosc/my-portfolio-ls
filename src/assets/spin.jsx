fd82dd0cac13a3151abf4061b0e09e83e44ac90a
Lucianosc <scaminaci.luciano@gmail.com>
2023-05-25 16:09:10 -0300

    Added Lottie player & animation & spinner

---
+++ src/assets/spin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function spin() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 472 471"
+      shapeRendering="geometricPrecision"
+    >
+      <path
+        d="M249.991 467.581c128.175-7.707 225.833-117.862 218.126-246.036S350.256-4.288 222.081 3.419-3.752 121.281 3.955 249.455s117.862 225.833 246.036 218.126z"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeDasharray="18.66,18.66"
+      />
+    </svg>
+  );
+}
