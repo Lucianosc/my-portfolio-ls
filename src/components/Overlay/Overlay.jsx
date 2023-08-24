cc4ddd1cb150f3040b192dbc134c278daee7f896
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-08-24 14:05:05 -0300

    fix logo & hamburger menu

--- src/components/Overlay/Overlay.jsx
+++ src/components/Overlay/Overlay.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./Overlay.module.scss";
 
-//Overlay z-index: 5
-
 export default function Overlay({ handleClick }) {
   return (
     <div className={`${styles["overlay"]}`} onClick={() => handleClick()}></div>
